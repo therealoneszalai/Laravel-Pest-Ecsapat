6604bdf1f920681e914ff0d5d4ba32498c2bc5ee
therealoneszalai <szalai.mark@students.jedlik.eu>
2026-02-01 17:32:26 +0100

    GetAll véglegesítés

--- tests/Feature/GetAllTaskTest.php
+++ tests/Feature/GetAllTaskTest.php
@@ -8,11 +8,11 @@ uses(Illuminate\Foundation\Testing\RefreshDatabase::class);
 
 it('kilistázza a bejelentkezett felhasználó összes feladatát', function () {
     $user = User::factory()->create();
-    
-    // Létrehozunk 3 saját feladatot
+
+    // 3 user create
     Task::factory()->count(3)->create(['user_id' => $user->id]);
-    
-    // Létrehozunk 2 "idegen" feladatot is a rendszerben
+
+    // Létrehozunk 2 taskot 
     Task::factory()->count(2)->create();
 
     actingAs($user)
